1ccd0f0c92166403773dc1edeaf75c5910f57736
ratherlongname <shivam.bansal@live.com>
2020-08-09 08:40:48 +0530

    Rename var to be more appropriate

--- pages/error.js
+++ pages/error.js
@@ -1,14 +1,14 @@
-export default function Error({ data }) {
+export default function Error({ src_url }) {
     return (
         <>
         <h1>Error!</h1>
-        <p><strong>Referrer:</strong> {data}</p>
+        <p><strong>Referrer:</strong> {src_url}</p>
         </>
     )
 }
 
 export async function getServerSideProps({req}) {
-    const data = req.headers.referer ? req.headers.referer : "no referer available";
-    console.log("Referer: ", data)
-    return {props: { data }};
+    const src_url = req.headers.referer ? req.headers.referer : "no referer available";
+    console.log("Referer: ", src_url)
+    return {props: { src_url }};
 }
